a59945afc8748d932893bebcaefd84eec141f4ae
Simon B.Stirling <simonsbs@gmail.com>
2023-09-10 18:31:08 +0300

    .

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+
+
+// this is a variable for name
+string name = "Bob";
+string lastName = "Smith"; // this is a variable for last name
+
+
+string thePlaceILoveToGo = "Home";
+// the variable above is a place i like to go
+
+string livesacathas = "Cat";
+string IMAVARIABLE = "Cat";
+
+string my9Lives = "Cat";
+string myNineLives = "Cat";
+
+string Name = "Jim";
+
+string Class = "Cat";
+
+// Comments:
+// RTFM - Read The F**** Message
+
+/*sdfsdf
+  sdffsdfs
+  
+  dfsdfsdf
+  sdfsdfsdf
+  sdfsdf
+ */ 
