a8f5a3ee948c9ae92d3ef4fecf7af9a37c0e4757
Simon B.Stirling <simonsbs@gmail.com>
2023-09-27 18:40:25 +0300

    .

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -140,4 +140,10 @@ Console.WriteLine("bye");
 // Write a program that outputs the times table for numbers 1-10
 // as a bonus - allow the user to input the number of times
 // tables to output
-
+/*
+1 2  3  4  5
+2 4  6  8  10
+3 6  9  12 15
+4 8  12 16 20
+5 10 15 20 25
+*/
